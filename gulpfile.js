0901ac70b85a0a48304ae5099e4d8abbc0a1a6cc
Filipe Linhares <lip.linhares@gmail.com>
2015-05-01 20:21:00 -0300

    Adds scss linter and postcss tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var size = require('gulp-size');
+var nano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var scsslint = require('gulp-scss-lint');
+var autoprefixer = require('gulp-autoprefixer');
 
 var dir = {
   jsFolder: 'assets/javascript',
@@ -11,5 +16,22 @@ gulp.task('sass', function() {
   .on('error', function(err) {
     console.error('Error!', err.message);
   })
+  .pipe(rename('main-unprefixed.css'))
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sass:lint', function () {
+  gulp.src('assets/stylesheets/**/*.scss')
+  .pipe(scsslint());
+});
+
+gulp.task('post:css', function() {
+  gulp.src('dist/css/main-unprefixed.css')
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions', 'ie 8', 'ie 9']
+  }))
+  .pipe(nano())
+  .pipe(rename('main.min.css'))
+  .pipe(size({ showFiles: true }))
   .pipe(gulp.dest('dist/css'));
 });
